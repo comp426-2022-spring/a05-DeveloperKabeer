f9d48215df876de5e1e3420867501647c6aeaf1b
TamableVirus <kkalra@email.unc.edu>
2022-04-30 03:21:50 -0400

    work on js flipcoin function

--- public/main.js
+++ public/main.js
@@ -1,5 +1,20 @@
 // Focus div based on nav button click
+const coin = document.getElementById("coin") 
+coin.addEventListener("click", flipCoin)
 
+
+async function flipCoin() {
+    const ep = "app/flip"
+    const url = document.baseURI + ep
+    await fetch(url)
+        .then(function(response) {
+            return response.json()
+        }).then(function(result) {
+            document.getElementById("result").innerHTML = result.flip 
+            document.getElementById("quarter").setAttribute("src", "assets/img/" + result.flip + ".png")
+        })
+
+}
 
 // Flip one coin and show coin image to match result when button clicked
 
@@ -7,6 +22,3 @@
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button//
-
-
-
